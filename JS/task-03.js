a9211b1edf6e3ae61a7bc5993e6aaac874a111f3
Nabok Serhii <s.nabock@gmail.com>
2021-06-28 23:51:19 +0300

    Update task-03.js

--- JS/task-03.js
+++ JS/task-03.js
@@ -27,14 +27,14 @@ const logError = (id) => {
 };
 
 /*
- * было
+ * Работает так
  */
 // makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
 // makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
 // makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
 // makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
 /*
- * стало
+ * Должно работать так
  */
 makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
 
